998612d256229412fbc8a0269a1b59dfd5becd90
TheLordSide <cedricaguigah@gmail.com>
2022-07-03 00:41:38 +0200

    Loginscreen added

---
+++ app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
@@ -0,0 +1,11 @@
+package tg.intaonline.intaonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
