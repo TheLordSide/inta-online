2196d6020f68bce85314932277c85080b2e3e98d
TheLordSide <cedricaguigah@gmail.com>
2022-07-03 16:51:16 +0200

    Correction LoginScreen

--- app/src/main/java/tg/intaonline/intaonline/SplashScreenActivity.kt
+++ app/src/main/java/tg/intaonline/intaonline/SplashScreenActivity.kt
@@ -18,7 +18,7 @@ class SplashScreenActivity : AppCompatActivity() {
         val ivgo=findViewById<View>(R.id.iv_go)
         ivgo.alpha= 0f
         ivgo.animate().setDuration(2000).alpha(1f).withEndAction{
-            val oi =  Intent(this, LoginScreenActivity::class.java)
+            val oi =  Intent(this, MainActivity::class.java)
             startActivity(oi)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
             finish()
