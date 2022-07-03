ddffcac0d1c59ea7b82b9e34dc9a93ae8322977d
TheLordSide <cedricaguigah@gmail.com>
2022-07-03 16:18:45 +0200

    Correction LoginScreen

--- app/src/main/java/tg/intaonline/intaonline/SplashScreenActivity.kt
+++ app/src/main/java/tg/intaonline/intaonline/SplashScreenActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
+        //set splashscreen to fullscreen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //animate the logo and launch loginscreen
         val ivgo=findViewById<View>(R.id.iv_go)
         ivgo.alpha= 0f
         ivgo.animate().setDuration(2000).alpha(1f).withEndAction{
