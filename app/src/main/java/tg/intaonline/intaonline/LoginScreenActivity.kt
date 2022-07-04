02b0a0fbb81179df1a78d341f2ffe478e12826da
TheLordSide <cedricaguigah@gmail.com>
2022-07-04 22:44:43 +0200

    Connexion marche

--- app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
+++ app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
@@ -3,24 +3,24 @@ package tg.intaonline.intaonline
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-
 import android.widget.Button
+import android.widget.EditText
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginScreenActivity : AppCompatActivity() {
 
-
+  private lateinit var binding: LoginScreenActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
         val connectbtn = findViewById<Button>(R.id.Connexion)
-        val userNameedit= findViewById<TextInputLayout>(R.id.UserName)
+        val usernametext = findViewById<EditText>(R.id.usermail)
 
-        if(TextUtils.isEmpty(userNameedit.toString())){
-            print("fuck")
-        }
+      connectbtn.setOnClickListener{
+
+      }
 
 
     }
