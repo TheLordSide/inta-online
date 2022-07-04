eff21a4d773ebbec9004cf09792c8af38d6a0a36
TheLordSide <cedricaguigah@gmail.com>
2022-07-04 21:17:54 +0200

    Login screen modifier 3

--- app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
+++ app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
@@ -2,11 +2,25 @@ package tg.intaonline.intaonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginScreenActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        val connectbtn = findViewById<Button>(R.id.Connexion)
+        val userNameedit= findViewById<TextInputLayout>(R.id.UserName)
+
+        if(TextUtils.isEmpty(userNameedit.toString())){
+            print("fuck")
+        }
 
 
     }
