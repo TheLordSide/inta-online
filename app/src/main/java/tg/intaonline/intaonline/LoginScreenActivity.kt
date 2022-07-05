6a8b54220a8c0857e008a6bdd8c119967b6a290b
TheLordSide <cedricaguigah@gmail.com>
2022-07-05 02:42:57 +0200

    END of Login screen front end

--- app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
+++ app/src/main/java/tg/intaonline/intaonline/LoginScreenActivity.kt
@@ -5,23 +5,46 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-
 class LoginScreenActivity : AppCompatActivity() {
 
-  private lateinit var binding: LoginScreenActivity
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
         val connectbtn = findViewById<Button>(R.id.Connexion)
-        val usernametext = findViewById<EditText>(R.id.usermail)
+
 
       connectbtn.setOnClickListener{
-
+        VerificationChamps()
       }
 
 
     }
+
+    private fun VerificationChamps(){
+        val email = findViewById<EditText>(R.id.usermail)
+        val pass = findViewById<EditText>(R.id.userPass)
+        val errortext = findViewById<TextInputLayout>(R.id.UserName)
+        val errortext2 = findViewById<TextInputLayout>(R.id.PassWord)
+        if (TextUtils.isEmpty(email.text)){
+
+            errortext.error = "Email vide"
+        }
+        else{
+            errortext.isErrorEnabled =false
+        }
+
+        if (TextUtils.isEmpty(pass.text)){
+
+            errortext2.error = "Mot de passe vide"
+        }
+        else{
+            errortext2.isErrorEnabled =false
+        }
+
+
+    }
 }
+
